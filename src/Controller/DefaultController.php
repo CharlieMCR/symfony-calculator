1fb2fb20a1bd739e2511122b1f48cc3d35060a8c
charliemcr <charlie@charliemcr.com>
2021-08-08 21:43:40 +0100

    Fixes missed standards

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
